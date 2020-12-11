6bd19cbde11f08fd46242d69ffe6b21a373f72b9
Li Weinan <li.weinan@rea-group.com>
2020-12-11 17:24:16 +0800

    add tw dns

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ provider "google" {
   region  = "asia-east2"
   zone    = "asia-east2-a"
   version = "~> 3.41"
-  alias = "hk"
 }
 provider "google" {
   project = var.project_id
@@ -25,7 +24,7 @@ module "ss-instance" {
   ss_password = var.ss_password
   instance_alias = "hk"
   providers = {
-    google.gcp = google.hk
+    google.gcp = google
   }
 }
 module "ss-instance-2" {
@@ -40,10 +39,36 @@ module "ss-instance-2" {
 resource "google_dns_managed_zone" "zone" {
   name = "ladder"
   dns_name = "ladder.li-weinan.com."
+  dnssec_config {
+    kind          = "dns#managedZoneDnsSecConfig" 
+    non_existence = "nsec3"
+    state         = "on" 
+
+    default_key_specs {
+      algorithm  = "rsasha256" 
+      key_length = 2048
+      key_type   = "keySigning"
+      kind       = "dns#dnsKeySpec" 
+    }
+    default_key_specs {
+      algorithm  = "rsasha256"
+      key_length = 1024 
+      key_type   = "zoneSigning" 
+      kind       = "dns#dnsKeySpec"
+    }
+  }
 }
 
 resource "google_dns_record_set" "dns" {
   name = "ladder.li-weinan.com."
+  type = "A"
+  ttl = 300
+  managed_zone = "ladder"
+  rrdatas = [module.ss-instance.ip_address]
+}
+
+resource "google_dns_record_set" "dns-tw" {
+  name = "tw.ladder.li-weinan.com."
   type = "A"
   ttl = 300
   managed_zone = "ladder"
